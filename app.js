cf9d6a698cd370627a2074732c2cb1d3be903c6e
Daniel Singer <dannys430@yahoo.com>
2020-08-23 11:10:47 -0400

    added passport.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
 const users = require('./routes/api/users')
 const posts = require('./routes/api/posts')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') // tells our app which requests to respond to
+const User = require('./models/User')
 
 mongoose
   .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -12,7 +13,13 @@ mongoose
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  console.log(res)
+  const user = new User({
+    handle: 'jim',
+    email: 'jim@jim.jim',
+    password: 'jimisgreat123'
+  })
+  user.save()
+  // console.log(res)
   res.send('How you doin, World?!?! Okurrr!!!')
 })
 
